6c5e3c4e95b6c5e54c6c25ee68e5022a0c104ae8
Funmisho <91758696+Funmisho@users.noreply.github.com>
2025-02-05 22:56:05 +0100

    Update app.py for zappa deployment

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import requests
 import math
 
 app = Flask(__name__)
+CORS(app)
 
 # Function to check if a number is prime
 def is_prime(n):
